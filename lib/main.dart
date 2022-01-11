3d6e4560f4250e8356a8d54111dbf172cb5754f9
Cristian Stratulat <Cyber@Cristians-MacBook-Pro.local>
2022-01-11 18:47:27 +0200

    test

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(csaf());
+
+class csaf extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+        home: Scaffold(
+            body: Container(
+                child: Text('Working'),
+                ),
+            ),
+        );
+  }
+}
+
